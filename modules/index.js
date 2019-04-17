baf9776075f6d169dba07d6b45a875f826060ac0
BarryBlando <barryblando@gmail.com>
2019-04-17 10:26:50 +0800

    Deleted

--- modules/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var circle_1 = require("./math/circle");
-console.log(circle_1.PI);
-circle_1.calculateCircumference(2);
--- namespaces/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-var CircleMath = MyMath.Circle;
-MyMath.calculateRectangle(25, 30);
-CircleMath.calculateCircumference(2);
